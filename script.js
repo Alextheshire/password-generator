3d31f455e951082db533b8358c738a84ef21dbc3
alexberes <aalexbberes@gmail.com>
2021-09-22 09:58:03 -0700

    removed excess files

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var num = [0,1,2,3,4,5,6,7,8,9]
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upper = lower.map(lower => lower.toUpperCase())
 var special = ["`","`","!","@","#","$","%","^","&","*","(",")","_","-","=","+","[","{","}","]",":",";","'","<",",",">",".","/","?","|","\\","\""]
-var charSet = []
 
 function generatePassword() {
   var charSet = []
